ca3a5bb46c582384f2e0427a08596d7ea21da1c6
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-19 16:13:09 -0400

    total assignment submission in cohorts-ordered

--- 3_queries_group-by/4_total_assignment_submissions.sql
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -1,6 +1,6 @@
-SELECT cohorts.name as cohort, sum(duration) as total_submission
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submission
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
 GROUP BY cohorts.name
-ORDER BY total_submission
+ORDER BY total_submission DESC
