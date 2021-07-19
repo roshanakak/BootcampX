9255f56cd363194f3a307b89764c06edeadd2c68
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-19 16:26:28 -0400

    low average completion time

---
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimatated_duration
+FROM students
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+JOIN assignments ON assignments.id = assignment_submissions.assignment_id
+WHERE end_date IS NULL
+GROUP BY students.name
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
